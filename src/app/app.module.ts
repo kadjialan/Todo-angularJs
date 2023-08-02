454c9f8c2c6835d7e7b8f7a928aec51dd3b61634
kadjialan <alankadji7@gmail.com>
2023-08-02 15:41:23 +0100

    implementing form submission

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,8 +13,10 @@ import { TodolistComponent } from './todolist/todolist.component';
     TodolistComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.css']
+  styleUrls: ['./todolist.component.css'],
 })
-export class TodolistComponent {
+export class TodolistComponent implements OnInit {
+  trash = faTrashCan
+ taskArray = [{taskName: 'pray to God', isCompleted:false}]
+  constructor() { }
 
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form)
+  }
 }
