2e50b43ad8b397d42ce783ea65c779ca06760d6b
kadjialan <alankadji7@gmail.com>
2023-08-03 01:17:42 +0100

    delete function and complete logique implementation

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
@@ -7,15 +7,21 @@ import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './todolist.component.html',
   styleUrls: ['./todolist.component.css'],
 })
-export class TodolistComponent implements OnInit {
+export class TodolistComponent {
   trash = faTrashCan
  taskArray = [{taskName: 'pray to God', isCompleted:false}]
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
   onSubmit(form: NgForm) {
     console.log(form)
+    this.taskArray.push({taskName: form.controls['task'].value, isCompleted:false})
+    form.reset()
+  }
+
+  onDelete(index: number) {
+    this.taskArray.splice(index, 1)
+  }
+
+  onCheck(index: number) {
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted
   }
 }
